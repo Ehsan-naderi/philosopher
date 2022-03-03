e7d6139445dfc5232606e957fd42c9526e570f37
mnaderi <mnaderi@student.42wolfsburg.de>
2022-03-03 10:42:48 +0100

    sdsd

--- utils.c
+++ utils.c
@@ -6,8 +6,10 @@
 /*   By: mnaderi <mnaderi@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 23:39:23 by mnaderi           #+#    #+#             */
-/*   Updated: 2022/02/04 02:26:32 by mnaderi          ###   ########.fr       */
+/*   Updated: 2022/03/03 10:42:19 by mnaderi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+//cherto pert ki mige?
